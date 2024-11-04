1d17217ee8f71b6523bbb66920c33793c991303f
naeemnoman <naeemnoman0419@gmail.com>
2024-11-04 10:01:00 +0600

    project done

---
+++ app/src/main/java/com/naeemnoman/demostaion2/Book.kt
@@ -0,0 +1,14 @@
+package com.naeemnoman.demostaion2
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "book_table")
+data class Book(
+   @PrimaryKey(autoGenerate = true) val id: Long = 0,
+   val title: String,
+   val author: String,
+   val pages: Int,
+   var isRead: Boolean = false
+)
